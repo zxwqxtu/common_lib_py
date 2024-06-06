e8a78057ac38f2711bb92e96277d6173aa49e4d7
王强 <wangqiang2@mgtv.com>
2024-06-06 11:09:33 +0800

    update

---
+++ src/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from setuptools import find_packages, setup
+
+setup(
+    name='common_lib_zxwqxtu',
+    version="0.0.2",
+    description='A small example package',
+    long_description=open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'README.md'), encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    keywords=['parseInt', 'parseString', 'implode', 'excelXlsx', 'http.get'],
+    author='zxwqxtu',
+    author_email='zxwqxtu@outlook.com',
+    maintainer='zxwqxtu',
+    url='https://github.com/zxwqxtu/common_lib_py',
+    packages=find_packages(include=['common_lib_zxwqxtu*']),
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+    ],
+    #install_requires=['selenium ~= 4.12'],
+)
